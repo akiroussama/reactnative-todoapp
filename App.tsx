0dcdd713e632f60ae6d0f225d25a1130ecd90ae2
Akir Oussama <akir.oussama@gmail.com>
2022-09-28 03:07:51 +0200

    create main screen

--- App.tsx
+++ App.tsx
@@ -1,21 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import AppContainer from './src/components/app-container';
+import MainScreen from './src/screens/main-screen';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text> Hello les wilders !! </Text>
-      <Text> Step 3: add the app container </Text>
-      <StatusBar style="auto" />
-    </View>
+    <AppContainer>
+     <MainScreen></MainScreen>
+    </AppContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
