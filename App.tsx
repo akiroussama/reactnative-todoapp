fb16e3930da5d0b3df29b45659076a00e246189f
Akir Oussama <akir.oussama@gmail.com>
2022-09-28 02:53:17 +0200

    add first component ( the app container )

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text> Hello les wilders !! </Text>
+      <Text> Step 3: add the app container </Text>
       <StatusBar style="auto" />
     </View>
   );
