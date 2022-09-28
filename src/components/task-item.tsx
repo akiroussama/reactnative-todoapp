8cafc0572e77150711619cb2474ef4bf1bc9f71f
Akir Oussama <akir.oussama@gmail.com>
2022-09-28 05:14:31 +0200

    create task item

---
+++ src/components/task-item.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import AnimatedCheckbox from 'react-native-checkbox-reanimated'
+import {
+  Box, HStack, Pressable,Text,
+} from 'native-base'
+
+interface Props {
+  isDone: boolean,
+  onToggleCheckbox?:()=> void
+}
+
+const TaskItem = (props: Props) => {
+  const { isDone,  onToggleCheckbox} = props
+
+  return (<HStack>
+  <Box width={30} height={30} mr={2}>
+    <Pressable onPress={onToggleCheckbox}>
+      <AnimatedCheckbox
+        highlightColor="#4444ff"
+        checkmarkColor="#ffffff"
+        boxOutlineColor="#4444ff"
+        checked={isDone}
+      />
+    </Pressable>
+  </Box>
+  <Text>Task item</Text>
+  </HStack>)
+
+}
+
+export default TaskItem
--- src/screens/main-screen.tsx
+++ src/screens/main-screen.tsx
@@ -1,14 +1,19 @@
 import { View, Text } from 'react-native';
 import { Center, Box } from 'native-base';
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import TaskItem from '../components/task-item';
 
 export default function MainScreen() {
+    const [checked, setChecked] = useState(false);
+    const handleToggleCheckbox = useCallback(() => {
+      }, [])
     return (
         <Center
           
           flex={1}>
             <Box>
                 <Text>main-screen</Text>
+                <TaskItem isDone={checked}  onToggleCheckbox={handleToggleCheckbox}></TaskItem>
             </Box>
         </Center>
     )
