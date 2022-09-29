5af9fb284e8f92e1fce43b34ae5d63ed5ec82cc2
Akir Oussama <akir.oussama@gmail.com>
2022-09-29 04:05:22 +0200

    fix syntax issue

--- src/index.tsx
+++ src/index.tsx
@@ -8,15 +8,8 @@ const Drawer = createDrawerNavigator()
 const App = () => {
   return (
     <Drawer.Navigator
-       initialRouteName="Main"
-    //   drawerContent={props => <Sidebar {...props} />}
-    //   screenOptions={{
-    //     headerShown: false,
-    //     drawerType: 'back',
-    //     overlayColor: '#00000000'
-    //   }}
-    >
-      <Drawer.Screen name="Main" component={MainScreen} />
+       initialRouteName="Main">
+       <Drawer.Screen name="Main" component={MainScreen} />
       <Drawer.Screen name="About" component={AboutScreen} />
     </Drawer.Navigator>
   )
